c548437f8ab5591a9f62c834c63c2fa3982a8bfb
yongseongjeon <yongseong.jeon95@gmail.com>
2021-12-15 12:04:34 +0900

    feat: show tab of add in the beginning

--- src/handler.js
+++ src/handler.js
@@ -1,21 +1,30 @@
-const tabAddEl = document.getElementById("tab-add");
-const tabManageEl = document.getElementById("tab-manage");
-const tabPurchaseEl = document.getElementById("tab-purchase");
+let tabAddEl = document.getElementById("tab-add");
+let tabManageEl = document.getElementById("tab-manage");
+let tabPurchaseEl = document.getElementById("tab-purchase");
 
 export const showTabAdd = () => {
+  updateTabEl();
   tabAddEl.style.display = "block";
   tabManageEl.style.display = "none";
   tabPurchaseEl.style.display = "none";
 };
 
 export const showTabManage = () => {
+  updateTabEl();
   tabAddEl.style.display = "none";
   tabManageEl.style.display = "block";
   tabPurchaseEl.style.display = "none";
 };
 
 export const showTabPurchase = () => {
+  updateTabEl();
   tabAddEl.style.display = "none";
   tabManageEl.style.display = "none";
   tabPurchaseEl.style.display = "block";
+};
+
+const updateTabEl = () => {
+  tabAddEl = document.getElementById("tab-add");
+  tabManageEl = document.getElementById("tab-manage");
+  tabPurchaseEl = document.getElementById("tab-purchase");
 };
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ class VendingMachine {
 
   init() {
     this.drawView();
+    showTabAdd();
     this.handleTabMovement();
   }
 
